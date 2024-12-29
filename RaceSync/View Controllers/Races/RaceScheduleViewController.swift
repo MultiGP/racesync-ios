f837796d2db3bceb660f9a6be43d5b194e4be91c
Ignacio Romero Zurbuchen <590579+dzenbot@users.noreply.github.com>
2024-12-29 01:10:52 -0800

    Adding the Schedule tab (disabled) to the race detail View

---
+++ RaceSync/View Controllers/Races/RaceScheduleViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RaceScheduleViewController.swift
+//  RaceSync
+//
+//  Created by Ignacio Romero Zurbuchen on 2024-12-29.
+//  Copyright © 2024 MultiGP Inc. All rights reserved.
+//
+
+import Foundation
