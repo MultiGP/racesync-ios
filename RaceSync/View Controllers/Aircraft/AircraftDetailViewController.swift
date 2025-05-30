82d8e0a6ec40668d50b938a920f5041ff52140d3
Ignacio Romero Zurbuchen <590579+dzenbot@users.noreply.github.com>
2025-05-29 18:16:46 -0700

    Minor UI adjustment on login screen

--- RaceSync/View Controllers/Aircraft/AircraftDetailViewController.swift
+++ RaceSync/View Controllers/Aircraft/AircraftDetailViewController.swift
@@ -54,7 +54,7 @@ class AircraftDetailViewController: UIViewController {
     fileprivate lazy var deleteButton: ActionButton = {
         let button = ActionButton(type: .system)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 21, weight: .regular)
-        button.setTitleColor(Color.red, for: .normal)
+        button.setTitleColor(Color.blue, for: .normal)
         button.setTitle("Retire Aircraft", for: .normal)
         button.backgroundColor = Color.white
         button.layer.cornerRadius = Constants.padding/2
