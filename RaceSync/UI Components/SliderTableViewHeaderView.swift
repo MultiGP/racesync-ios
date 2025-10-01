1e8b0cfd71be6902a617957f6cd1594ef97fee33
Ignacio Romero Zurbuchen <590579+dzenbot@users.noreply.github.com>
2025-09-30 19:01:12 -0700

    Fixd tab bar not always been selected

---
+++ RaceSync/UI Components/SliderTableViewHeaderView.swift
@@ -0,0 +1,9 @@
+//
+//  SliderTableViewHeaderView.swift
+//  RaceSync
+//
+//  Created by Ignacio Romero Zurbuchen on 2025-09-27.
+//  Copyright © 2025 MultiGP Inc. All rights reserved.
+//
+
+import Foundation
